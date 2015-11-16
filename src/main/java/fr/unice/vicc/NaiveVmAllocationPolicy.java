36fc59feb494e61cf1c5f37015b4486e00777bd9
Fabien Hermenier <fabien.hermenier@unice.fr>
2015-11-16 10:03:55 +0100

    skeleton

---
+++ src/main/java/fr/unice/vicc/NaiveVmAllocationPolicy.java
@@ -0,0 +1,59 @@
+package fr.unice.vicc;
+
+import org.cloudbus.cloudsim.Host;
+import org.cloudbus.cloudsim.Vm;
+import org.cloudbus.cloudsim.VmAllocationPolicy;
+import org.cloudbus.cloudsim.power.PowerHost;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by fhermeni2 on 16/11/2015.
+ */
+public class NaiveVmAllocationPolicy extends VmAllocationPolicy {
+
+    public NaiveVmAllocationPolicy(List<? extends Host> list) {
+        super(list);
+    }
+
+    @Override
+    protected void setHostList(List<? extends Host> hostList) {
+        super.setHostList(hostList);
+    }
+
+    @Override
+    public <T extends Host> List<T> getHostList() {
+        return super.getHostList();
+    }
+
+    @Override
+    public boolean allocateHostForVm(Vm vm) {
+        return false;
+    }
+
+    @Override
+    public boolean allocateHostForVm(Vm vm, Host host) {
+        return false;
+    }
+
+    @Override
+    public List<Map<String, Object>> optimizeAllocation(List<? extends Vm> list) {
+        return null;
+    }
+
+    @Override
+    public void deallocateHostForVm(Vm vm) {
+
+    }
+
+    @Override
+    public Host getHost(Vm vm) {
+        return null;
+    }
+
+    @Override
+    public Host getHost(int i, int i1) {
+        return null;
+    }
+}
